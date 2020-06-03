ba1f17da751c2ad0298052769814c9f96efee24a
Ethan Dalool <git@voussoir.net>
2020-06-03 13:00:44 -0700

    Move function add_channel to api.js.

--- frontends/ycdl_flask/static/js/api.js
+++ frontends/ycdl_flask/static/js/api.js
@@ -3,6 +3,15 @@ var api = {};
 /**************************************************************************************************/
 api.channels = {};
 
+api.channels.add_channel =
+function add_channel(channel_id, callback)
+{
+    var url = "/add_channel";
+    data = new FormData();
+    data.append("channel_id", channel_id);
+    return common.post(url, data, callback);
+}
+
 api.channels.refresh_channel =
 function refresh_channel(channel_id, force, callback)
 {
