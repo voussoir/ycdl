66f499c232ab8be414275763310040b26921c43f
Ethan Dalool <git@voussoir.net>
2020-06-03 13:01:53 -0700

    Move functions mark_state, start_download to api.js.

--- frontends/ycdl_flask/static/js/api.js
+++ frontends/ycdl_flask/static/js/api.js
@@ -41,3 +41,22 @@ function set_automark(channel_id, state, callback)
 
 /**************************************************************************************************/
 api.videos = {};
+
+api.videos.mark_state =
+function mark_state(video_ids, state, callback)
+{
+    var url = "/mark_video_state";
+    data = new FormData();
+    data.append("video_ids", video_ids);
+    data.append("state", state);
+    return common.post(url, data, callback);
+}
+
+api.videos.start_download =
+function start_download(video_ids, callback)
+{
+    var url = "/start_download";
+    data = new FormData();
+    data.append("video_ids", video_ids);
+    return common.post(url, data, callback);
+}
