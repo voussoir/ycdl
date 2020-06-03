ab1a2364bece864f199526edcfd4bd96b6ba6673
Ethan Dalool <git@voussoir.net>
2020-06-03 12:59:29 -0700

    Move function refresh_channel to api.js.

--- frontends/ycdl_flask/static/js/api.js
+++ frontends/ycdl_flask/static/js/api.js
@@ -3,5 +3,14 @@ var api = {};
 /**************************************************************************************************/
 api.channels = {};
 
+api.channels.refresh_channel =
+function refresh_channel(channel_id, force, callback)
+{
+    var url = `/channel/${channel_id}/refresh`;
+    data = new FormData();
+    data.append("force", force)
+    return common.post(url, data, callback);
+}
+
 /**************************************************************************************************/
 api.videos = {};
