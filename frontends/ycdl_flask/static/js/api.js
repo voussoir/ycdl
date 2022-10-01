2bd8f42eb0624d456dfa28aac9e1cd8010a861b0
Ethan Dalool <git@voussoir.net>
2022-10-01 14:30:08 -0700

    Move http functions to new javascript file http.js.

--- frontends/ycdl_flask/static/js/api.js
+++ frontends/ycdl_flask/static/js/api.js
@@ -6,80 +6,100 @@ api.channels = {};
 api.channels.add_channel =
 function add_channel(channel_id, callback)
 {
-    const url = "/add_channel";
-    const data = {"channel_id": channel_id};
-    return common.post(url, data, callback);
+    return http.post({
+        url: "/add_channel",
+        data: {"channel_id": channel_id},
+        callback: callback,
+    });
 }
 
 api.channels.delete_channel =
 function delete_channel(channel_id, callback)
 {
-    const url = `/channel/${channel_id}/delete`;
-    const data = {};
-    return common.post(url, data, callback);
+    return http.post({
+        url: `/channel/${channel_id}/delete`,
+        data: {},
+        callback: callback,
+    });
 }
 
 api.channels.refresh_channel =
 function refresh_channel(channel_id, force, callback)
 {
-    const url = `/channel/${channel_id}/refresh`;
-    const data = {"force": force};
-    return common.post(url, data, callback);
+    return http.post({
+        url: `/channel/${channel_id}/refresh`,
+        data: {"force": force},
+        callback: callback,
+    });
 }
 
 api.channels.refresh_all_channels =
 function refresh_all_channels(force, callback)
 {
-    const url = "/refresh_all_channels";
-    const data = {"force": force};
-    return common.post(url, data, callback);
+    return http.post({
+        url: "/refresh_all_channels",
+        data: {"force": force},
+        callback: callback,
+    });
 }
 
 api.channels.set_automark =
 function set_automark(channel_id, state, callback)
 {
-    const url = `/channel/${channel_id}/set_automark`;
-    const data = {"state": state};
-    return common.post(url, data, callback);
+    return http.post({
+        url: `/channel/${channel_id}/set_automark`,
+        data: {"state": state},
+        callback: callback,
+    });
 }
 
 api.channels.set_autorefresh =
 function set_autorefresh(channel_id, autorefresh, callback)
 {
-    const url = `/channel/${channel_id}/set_autorefresh`;
-    const data = {"autorefresh": autorefresh};
-    return common.post(url, data, callback);
+    return http.post({
+        url: `/channel/${channel_id}/set_autorefresh`,
+        data: {"autorefresh": autorefresh},
+        callback: callback,
+    });
 }
 
 api.channels.set_download_directory =
 function set_download_directory(channel_id, download_directory, callback)
 {
-    const url = `/channel/${channel_id}/set_download_directory`;
-    const data = {"download_directory": download_directory};
-    return common.post(url, data, callback);
+    return http.post({
+        url: `/channel/${channel_id}/set_download_directory`,
+        data: {"download_directory": download_directory},
+        callback: callback,
+    });
 }
 
 api.channels.set_name =
 function set_name(channel_id, name, callback)
 {
-    const url = `/channel/${channel_id}/set_name`;
-    const data = {"name": name};
-    return common.post(url, data, callback);
+    return http.post({
+        url: `/channel/${channel_id}/set_name`,
+        data: {"name": name},
+        callback: callback,
+    });
 }
 
 api.channels.set_queuefile_extension =
 function set_queuefile_extension(channel_id, extension, callback)
 {
-    const url = `/channel/${channel_id}/set_queuefile_extension`;
-    const data = {"extension": extension};
-    return common.post(url, data, callback);
+    return http.post({
+        url: `/channel/${channel_id}/set_queuefile_extension`,
+        data: {"extension": extension},
+        callback: callback,
+    });
 }
 
 api.channels.show_download_directory =
 function show_download_directory(channel_id, callback)
 {
-    const url = `/channel/${channel_id}/show_download_directory`;
-    return common.post(url, null, callback);
+    return http.post({
+        url: `/channel/${channel_id}/show_download_directory`,
+        callback: callback,
+    });
 }
 
 api.channels.callback_go_to_channels =
@@ -101,15 +121,19 @@ api.videos = {};
 api.videos.mark_state =
 function mark_state(video_ids, state, callback)
 {
-    const url = "/mark_video_state";
-    const data = {"video_ids": video_ids, "state": state};
-    return common.post(url, data, callback);
+    return http.post({
+        url: "/mark_video_state",
+        data: {"video_ids": video_ids, "state": state},
+        callback: callback,
+    });
 }
 
 api.videos.start_download =
 function start_download(video_ids, callback)
 {
-    const url = "/start_download";
-    const data = {"video_ids": video_ids};
-    return common.post(url, data, callback);
+    return http.post({
+        url: "/start_download",
+        data: {"video_ids": video_ids},
+        callback: callback,
+    });
 }
